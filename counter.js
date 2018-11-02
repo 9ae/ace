06672d1131b3b64ed6622d60950bef07d86ac8ad
Alice Q. Wong <alice@valor.me>
2018-11-02 11:14:00 -0700

    auto wordcount update

--- counter.js
+++ counter.js
@@ -31,11 +31,55 @@ countDir('drabbles');
 results.push('\n**TOTAL = '+total+"**\n\n");
 
 var all = results.join('\n');
-fs.appendFile(
-  'notes/nanowrimo.md',
-  all,
-  'utf8',
-  function(err){ if (err) console.log(err) }
-)
+
+// Append to logfile
+if (process.argv.includes('log')) {
+  fs.appendFile(
+    'notes/nanowrimo.md',
+    all,
+    'utf8',
+    function(err){ if (err) console.log(err) }
+  )
+  console.log('append to log file')
+}
+
+// Update wordcount on nanowrimo
+if (process.argv.includes('send')) {
+  var key = process.env.NANO_KEY;
+  var user = process.env.NANO_USR;
+  if (!key || !user) {
+    console.log('missing env vars'); return;
+  }
+  if (!total) return;
+
+  var sha1 = require('sha1');
+  var req = require('request');
+
+  var url = 'https://nanowrimo.org/api/wordcount';
+  var combo = key + user + total;
+  var hash = sha1(combo);
+  var data = {
+      hash: hash,
+      name: user,
+      wordcount: total
+    };
+
+  console.log(data);
+  
+  req.put(
+    { url: url,
+      formData: data
+    }
+  , function (error, response, body) {
+      if(response.statusCode == 200){
+        console.log('updated nano word count');
+      } else {
+        console.log('error: '+ response.statusCode)
+        console.log(body)
+      }
+    }
+  )
+}
 
 console.log(all);
+
--- notes/nanowrimo.md
+++ notes/nanowrimo.md
@@ -5,3 +5,11 @@
 * alan_adolf.md:	115 words
 
 **TOTAL = 1233**
+11/2/2018, 11:12:48 AM
+* 01.md:	90 words
+* 02.md:	1025 words
+* 03.md:	12 words
+* alan_adolf.md:	115 words
+
+**TOTAL = 1242**
+
